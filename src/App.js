732080a6f5a5c0662932081fa7eefa124ffc3e41
Md.Mohtasim Shariar <onim04@gmail.com>
2021-03-02 16:40:32 +0600

    hi

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Players from './components/Player/Player';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+
+function App() {
+  return (
+    <div >
+      <Header></Header>
+      <Players></Players>
+    
+    </div>
+  );
+}
+
+export default App;
